dbea67011900329065de7cf1096c6daf708af6c1
Ricky Amparo <rickyamparo@gmail.com>
2017-11-01 11:02:13 -0600

    change hardcoded zipcode in search index view, now calling params zipcode to find zipcode that was entered

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    # raw_stores = BestBuyService.stores_by_zip(params[:zipcode])
-    #
-    # @stores = raw_stores[:stores].map do |store|
-    #   Store.new(store)
-    # end
     @stores = Store.find_by_zip(params[:zipcode])
   end
 
