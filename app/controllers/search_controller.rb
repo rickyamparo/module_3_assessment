0c8d2995fb65da55d2660e737cd7e07c645a8e22
Ricky Amparo <rickyamparo@gmail.com>
2017-11-01 10:39:48 -0600

    postman query working, added it to the search controller to get spec to go green

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,14 @@
-class SearchController <ApplicationController
+class SearchController < ApplicationController
 
   def index
-    params
-    @conn = Faraday.new()
+    @conn = Faraday.new(:url => "https://api.bestbuy.com/v1/stores?format=json&apiKey=ru99w3xp6qu5848qvbgrznxq") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+    response = @conn.get("https://api.bestbuy.com/v1/stores(area(80202,25))?format=json&show=storeId,storeType,name,phone,distance&apiKey=ru99w3xp6qu5848qvbgrznxq")
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    @stores = parsed_response[:stores].map do |store|
+      Store.new(store)
+    end
   end
 
 end
